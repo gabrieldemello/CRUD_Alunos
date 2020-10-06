b14f7e4b1d1aef7c668c9e9ebe047063c55e018b
gabrieldemello <camino300@gmail.com>
2020-10-05 22:08:59 -0300

    Crud Alunos Commit.

---
+++ CRUD_Alunos/CRUD_Alunos/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_Alunos
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CRUD_Alunos/CRUD_Alunos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CRUD_Alunos.MvcApplication" Language="C#" %>
